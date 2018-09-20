4fe663154adb15de95d0876af96522aaf0659463
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-09-20 09:10:01 +0900

    [add] add status bar

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
+
+const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Todo App dayo!</Text>
       </View>
     );
   }
@@ -15,7 +17,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: STATUSBAR_HEIGHT
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
