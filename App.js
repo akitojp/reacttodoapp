1e40b3fef39431f58d751b45065e9e910551a8dd
Akito Tsutsui <akito.tsutsui@gmail.com>
2018-09-20 10:34:49 +0900

    [add] todo Componentを追加

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Platform,
+  ScrollView,
+  FlatList,
+} from 'react-native';
 
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
 export default class App extends React.Component {
+  
+  constructor(props) {
+    super();
+    this.state = {
+      todo: [
+        {index: 1, title: 'appを作る', done: false},
+        {index: 2, title: '何かやる', done: false}
+      ],
+      currentIndex: 2
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Todo App dayo!</Text>
+        {/* Filter */}
+        <View style={styles.filter}>
+          <Text>filterを配置</Text>
+        </View>
+        {/* todolist */}
+        <ScrollView style={styles.todolist}>
+          {/* Flat listを実装 */}
+          <FlatList data={this.state.todo}
+            renderItem={({item}) => <Text>{item.title}</Text>}
+            keyExtractor={(item, index) => "todo_" + item.index}
+          />
+        </ScrollView>
+        {/* input area */}
+        <View style={styles.input}>
+          <Text>inputエリアを配置</Text>
+        </View>
       </View>
     );
   }
@@ -18,7 +53,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: STATUSBAR_HEIGHT
-    // alignItems: 'center',
-    // justifyContent: 'center',
+  },
+  filter: {
+    height: 30,
+  },
+  todolist: {
+    flex: 1,
+  },
+  input: {
+    height: 30,
   },
 });
